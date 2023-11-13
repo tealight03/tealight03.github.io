aeb0b17fd131ac9e00b6079fbc5c5144e3687f49
lightingtea <davin0706@gmail.com>
2023-11-13 10:04:29 +0900

    upload 10week files

---
+++ 10week/data.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def make_x_and_y(n):
+    x = np.random.randn(n)
+    y = np.random.randn(n)
+    return x, y
+
+
+from data import make_x_and_y
+
+x, y = make_x_and_y(n=1000)
+
+fig, ax = plt.subplots()
+ax.scatter(x, y)
+fig
